bc9705e093f7bb56bb6cf62a08c531f411d08692
agustina <abenavent@udesa.edu.ar>
2024-04-10 17:23:09 -0300

    id

--- ProyectoIntegrador/routes/product.js
+++ ProyectoIntegrador/routes/product.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 let productController = require('../controllers/productController')
 
-router.get('/', productController.index)
-router.get('/product/:id', productController.detalleProducto)
+// router.get('/', productController.index)
+// router.get('/product/:id', productController.detalleProducto)
 
 module.exports = router
