5312189485abb9ff55b0e1c1fc21fa19993038c4
GiuGar <gargiulogiu0@gmail.com>
2024-04-10 16:04:57 -0300

    falla

--- ProyectoIntegrador/controllers/productController.js
+++ ProyectoIntegrador/controllers/productController.js
@@ -4,6 +4,17 @@ const productController = {
     index: function(req, res){
         return res.render("product", {'data': data
     })
+    },
+    
+    detalleProducto: function (req, res) {
+        let idEnviado = req.params.id
+        for (let i = 0; i < data.productos.length; i++) {
+            if (idEnviado == data.productos[i].id) {
+                return res.render('product',{
+                    'producto': data.productos[i]
+                })
+            }
+        }
     }
 }
 
--- ProyectoIntegrador/routes/product.js
+++ ProyectoIntegrador/routes/product.js
@@ -4,5 +4,6 @@ const router = express.Router()
 let productController = require('../controllers/productController')
 
 router.get('/', productController.index)
+router.get('/product/:id', productController.detalleProducto)
 
 module.exports = router
