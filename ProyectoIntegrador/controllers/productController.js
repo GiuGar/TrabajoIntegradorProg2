0c286a13a09bcab13ceaac578748d5ccdcf2c4a9
GiuGar <gargiulogiu0@gmail.com>
2024-04-15 09:27:48 -0300

    arreglorutaproduct

--- ProyectoIntegrador/controllers/productController.js
+++ ProyectoIntegrador/controllers/productController.js
@@ -1,37 +1,35 @@
 let data = require('../db/index');
 
 const productController = {
-    index: function(req, res){
-        return res.render("product", {'data': data
+    detalleProducto: function (req, res) {
+        let idEnviado = req.params.id;
+        let detalleProducto = [];
+        let comentariosProducto = [];
+        for (let i = 0; i < data.productos.length; i++) {
+            if (idEnviado == data.productos[i].id) {
+                detalleProducto.push(data.productos[i]);
+                comentariosProducto = data.productos[i].comentarios; // Obtener comentarios del producto
+                break; // Terminar el bucle una vez encontrado el producto
+            }
+        }
+        return res.render('product', {
+            index: detalleProducto,
+            comentarios: comentariosProducto // Pasar los comentarios al render
         })
     },
+
     resultadosDeBusqueda: function(req, res){
-        return res.render("search-results", {'data': data
-        })
+        let buscar = req.query.busqueda
+
+        return res.render("search-results", {datoBuscado: buscar})
     },
+
     add: function(req,res) {
         
         return res.render("product-add", {
             productos: data.productos
         })
-
-
     }
 }
-
-    
-    // detalleProducto: function (req, res) {
-    //     let idEnviado = req.params.id
-    //     let detalleProducto = []
-    //     for (let i = 0; i < data.productos.length; i++) {
-    //         if (idEnviado == data.productos[i].id) {
-    //             detalleProducto.push(data.productos[i])
-    //             return res.render('product',{
-    //                 'producto': detalleProducto
-    //             })
-    //         }
-    //     }
-    // }
-
 
 module.exports = productController
