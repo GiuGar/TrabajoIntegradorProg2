ba42268c86ca008a7f4671fac827809820a1caac
GiuGar <gargiulogiu0@gmail.com>
2024-04-15 10:55:24 -0300

    1

--- ProyectoIntegrador/controllers/productController.js
+++ ProyectoIntegrador/controllers/productController.js
@@ -21,10 +21,6 @@ const productController = {
     resultadosDeBusqueda: function(req, res){
         let buscar = req.query.busqueda
 
-        for (let i = 0; i < data.productos.length; i++) {
-            
-        }
-
         return res.render("search-results", {datoBuscado: buscar})
     },
 
--- ProyectoIntegrador/controllers/userController.js
+++ ProyectoIntegrador/controllers/userController.js
@@ -5,10 +5,12 @@ const userController = {
         return res.render('profile', {"data": data
         })
     },
+    
     register: function(req, res){
         return res.render('register', {"data": data
         })
     },
+    
     login: function(req, res){
         return res.render('login', {"data": data
         })
