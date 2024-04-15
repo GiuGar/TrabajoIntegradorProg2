3851717bfdaee33ce2c7c8bb1962faed7bea68ce
agustina <abenavent@udesa.edu.ar>
2024-04-15 09:31:31 -0300

    Comentarios6

--- ProyectoIntegrador/controllers/productController.js
+++ ProyectoIntegrador/controllers/productController.js
@@ -19,19 +19,4 @@ const productController = {
     }
 }
 
-    
-    // detalleProducto: function (req, res) {
-    //     let idEnviado = req.params.id
-    //     let detalleProducto = []
-    //     for (let i = 0; i < data.productos.length; i++) {
-    //         if (idEnviado == data.productos[i].id) {
-    //             detalleProducto.push(data.productos[i])
-    //             return res.render('product',{
-    //                 'producto': detalleProducto
-    //             })
-    //         }
-    //     }
-    // }
-
-
 module.exports = productController
