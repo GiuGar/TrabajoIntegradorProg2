e7f05f3937e547a83e69d80714b29fb90821a81a
agustina <abenavent@udesa.edu.ar>
2024-06-10 09:42:22 -0300

    Modelo commentarios

---
+++ ProyectoIntegrador/database/models/Comment.js
@@ -0,0 +1,41 @@
+const { sequelize } = require(".");
+
+module.exports= function(sequelize,dataTypes){
+    let alias= "Comment";
+    let cols = {
+        id:{
+            autoIncrement : true,
+            primaryKey: true,
+            type: dataTypes.DECIMAL //Porque va del 1 al 20
+        },
+        id_usuario:{
+            type: dataTypes.INTEGER
+        },
+        id_producto:{
+            type: dataTypes.INTEGER
+        },
+        texto_comentario:{
+            type: dataTypes.STRING
+        },
+        createdAt:{
+            type: dataTypes.DATE
+        },
+        updatedAt:{
+            type: dataTypes.DATE
+        },
+        deletedAt:{
+            type:dataTypes.DATE
+        }
+
+    }
+    let config = {
+        tableName: "comentarios", //Agrego el nombre de la tabla
+        timestamps: true, //Coma pq son objetos literales
+        underscorded: true// Por si algun nombre tiene guion bajo
+    }
+
+
+let Comment = sequelize.define(alias,cols,config);
+    return Comment; // es una variable que va a
+
+}
