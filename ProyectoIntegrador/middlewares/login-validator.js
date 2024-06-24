67325f334b232068175d850824ef23fcf5dc8899
agustina <abenavent@udesa.edu.ar>
2024-06-24 00:27:49 -0300

    login terminado

--- ProyectoIntegrador/middlewares/login-validator.js
+++ ProyectoIntegrador/middlewares/login-validator.js
@@ -1,52 +1,39 @@
 const { body } = require("express-validator");
-const db = require("../database/models")
-const bcryptjs = require("bcryptjs")
+const db = require("../database/models");
+const bcryptjs = require("bcryptjs");
 
 const loginValidation = [
     body("email")
         .notEmpty()
         .withMessage("Debe ingresar su email")
-        // .bail()
+        .bail()
         .isEmail()
         .withMessage("Debe ser en formato email")
-        .custom(function(value, {req}){
-            return db.User.findOne({
-                where: {email: value},
-            })
-                .then(function(user){
-                    if(!user){
-                        throw new Error ("Este email no está registrado")                    
-                    } else{
-                        req.user = user
+        .custom(function(value) {
+            return db.User.findOne({ where: { email: value } })
+                .then(function(user) {
+                    if (!user) {
+                        throw new Error("Este email no está registrado");
                     }
-
                 })
-                .catch(function(error){
-                    console.log(error)
-                })
-    }),
-
+                
+        }),
 
     body("password")
-    .notEmpty()
-    .withMessage("Debes completar con una contraseña")
-    .custom(function(value,{req}){
-    
-        return db.User.findOne({
-            where: { email: req.body.email}
-        })
-        .then(function(user) {
-            if (user) {
-                const passwordValida = bcryptjs.compareSync(value, user.password)
-                if (!passwordValida) {
-                    throw new Error("Contraseña incorrecta")
-                }
-            }
-        })
-        .catch(function(error){
-           console.log(error)        })
-        
-    })        
-]
-module.exports = loginValidation;
+        .notEmpty()
+        .withMessage("Debes completar con una contraseña")
+        .custom(function(value, { req }) {
+            return db.User.findOne({ where: { email: req.body.email } })
+                .then(function(user) {
+                    if (user) {
+                        const passwordValida = bcryptjs.compareSync(value, user.password);
+                        if (!passwordValida) {
+                            throw new Error("Contraseña incorrecta");
+                        }
+                    }
+                })
+               
+        }),
+];
 
+module.exports = loginValidation;
