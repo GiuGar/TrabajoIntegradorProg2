2846c8cb70e2f3286e26c3bffe17d19c1390d708
GiuGar <gargiulogiu0@gmail.com>
2024-06-24 21:13:46 -0300

    !

--- ProyectoIntegrador/controllers/indexController.js
+++ ProyectoIntegrador/controllers/indexController.js
@@ -7,7 +7,6 @@ const indexController = {
         db.Product.findAll({
             include: [{association: "usuario"}],
             order:[["createdAt","DESC"]],
-            limit: 10
         })
         .then(function(data){  //Este then agarra el db.Product todos los productos y los pone en data
             res.render("index", {data: data} ) //Nos lleva a index.ejs y lo de la funcion data lo pone en data para usarlo en la vista
